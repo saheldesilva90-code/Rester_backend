c3c27cb4057ef066897a42b7a400ba714eaae7ae
maneshadesilva99-code <maneshadesilva99@gmail.com>
2026-04-03 10:48:32 +0530

    notification

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware";
-import { getMe, updateMe, searchUsers, getUserProfile } from "../controllers/userController";
+import { getMe, updateMe, searchUsers, getUserProfile, savePushToken } from "../controllers/userController";
 
 const router = Router();
 
 router.get("/me", authMiddleware, getMe);
 router.put("/me", authMiddleware, updateMe);
+router.post("/push-token", authMiddleware, savePushToken);
 router.get("/search", authMiddleware, searchUsers);
 router.get("/:userId", authMiddleware, getUserProfile);
 
